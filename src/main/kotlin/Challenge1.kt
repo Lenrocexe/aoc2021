f15765fb440e2ec9b5f4828eea28a956d8cc00a5
Cornel Alders <c.alders@homewizard.nl>
2021-12-01 12:20:36 +0100

    Challenge 1: part 2

--- src/main/kotlin/Challenge1.kt
+++ src/main/kotlin/Challenge1.kt
@@ -5,26 +5,41 @@ fun main() {
 
     var increases = 0
     var decreases = 0
+    var noChanges = 0
 
-    var previous: String? = null
-    File("src/main/resources/Input1.txt").forEachLine { line ->
-        if (previous == null) {
-            println("$line (N/A - no previous measurement)")
+    var previousWindow: Int? = null
+
+    val measurements = mutableListOf<Int>()
+    File("src/main/resources/Input1.txt").forEachLine { measurements.add(it.toInt()) }
+
+    for (index in measurements.indices) {
+        if (index + 3 > measurements.size) break
+
+        val window = measurements.subList(index, index + 3)
+        val result = window.reduce { a, b -> a + b }
+
+        if (previousWindow == null) {
+            println("$result (N/A - no previous measurement)")
         } else {
-            when (compareValuesBy(line, previous) { it?.toInt() }) {
+            when (compareValuesBy(result, previousWindow) { it }) {
                 1 -> {
-                    println("$line (increased)")
+                    println("$result (increased)")
                     increases++
                 }
                 -1 -> {
-                    println("$line (decreased)")
+                    println("$result (decreased)")
                     decreases++
+                }
+                0 -> {
+                    println("$result (no change)")
+                    noChanges++
                 }
             }
         }
-        previous = line
+        previousWindow = result
     }
 
     println("number of measurements larger then the previous: $increases")
     println("number of measurements smaller then the previous: $decreases")
+    println("number of measurements equal to the previous: $noChanges")
 }
