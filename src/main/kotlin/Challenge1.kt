34a3a410a77c9cb0f9d457db3fdc123e470dc695
Cornel Alders <c.alders@homewizard.nl>
2021-12-01 11:31:54 +0100

    Challenge1: Track number of changes

--- src/main/kotlin/Challenge1.kt
+++ src/main/kotlin/Challenge1.kt
@@ -3,16 +3,28 @@ import java.io.File
 fun main() {
     println("aoc021 Challenge 1\n")
 
+    var increases = 0
+    var decreases = 0
+
     var previous: String? = null
     File("src/main/resources/Input1.txt").forEachLine { line ->
         if (previous == null) {
             println("$line (N/A - no previous measurement)")
         } else {
             when (compareValuesBy(line, previous) { it?.toInt() }) {
-                1 -> println("$line (increased)")
-                -1 -> println("$line (decreased)")
+                1 -> {
+                    println("$line (increased)")
+                    increases++
+                }
+                -1 -> {
+                    println("$line (decreased)")
+                    decreases++
+                }
             }
         }
         previous = line
     }
+
+    println("number of measurements larger then the previous: $increases")
+    println("number of measurements smaller then the previous: $decreases")
 }
