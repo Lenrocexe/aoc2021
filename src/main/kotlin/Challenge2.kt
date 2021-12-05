df2a3c194e9336c2fef9042f3fb63e7585740ede
Cornel Alders <c.alders@homewizard.nl>
2021-12-05 11:51:43 +0100

    Challenge 2 part 2

--- src/main/kotlin/Challenge2.kt
+++ src/main/kotlin/Challenge2.kt
@@ -1,19 +1,22 @@
 import java.io.File
 
 data class Movement(var direction: String, var distance: Int)
-data class Position(var x: Int, var y: Int)
+data class Position(var x: Int, var y: Int, var aim: Int)
 
 fun main() {
     println("Challenge 2")
 
-    val position = Position(0, 0)
+    val position = Position(0, 0, 0)
     File("src/main/resources/input2.txt").forEachLine {
         val split = it.split(" ")
         with(Movement(split[0], split[1].toInt())) {
             when (direction) {
-                "forward" -> position.x += distance
-                "up"      -> position.y -= distance
-                "down"    -> position.y += distance
+                "forward" -> {
+                    position.x += distance
+                    position.y += position.aim * distance
+                }
+                "up"      -> position.aim -= distance
+                "down"    -> position.aim += distance
             }
         }
     }
