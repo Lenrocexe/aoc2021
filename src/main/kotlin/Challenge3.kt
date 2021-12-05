8b2604ea101824d5a2eeba3cf3cdd18ec936496f
Cornel Alders <c.alders@homewizard.nl>
2021-12-05 14:44:37 +0100

    Challenge 3 part 2

--- src/main/kotlin/Challenge3.kt
+++ src/main/kotlin/Challenge3.kt
@@ -2,40 +2,72 @@ import java.io.File
 
 data class BitFlags(var zeroes: Int, var ones: Int) {
     fun zeroesMostCommon() = zeroes > ones
+    fun zeroesLeastCommon() = zeroes < ones
+    fun equalNumbers() = zeroes == ones
+
+    fun mostCommon(precedence: Char? = null) =
+        if (equalNumbers() && precedence != null)
+            precedence
+        else if (zeroesMostCommon())
+            '0'
+        else
+            '1'
+
+    fun leastCommon(precedence: Char? = null) =
+        if (equalNumbers() && precedence != null)
+            precedence
+        else if (zeroesLeastCommon())
+            '0'
+        else
+            '1'
 }
 
 fun main() {
-    println("aoc021 Challenge 3\n")
+    println("aoc021 Challenge 3 part 2\n")
 
-    var gammaRate = ""
-    var epsilonRate = ""
+    val lines = File("src/main/resources/input3.txt").readLines()
 
-    var buckets: List<BitFlags>? = null
+    val oxygen = getOxygen(lines, '1')
+    val co2scrubber = getCO2Scrubber(lines, '0')
 
-    File("src/main/resources/input3.txt").forEachLine {
+    println("oxygen              = $oxygen")
+    println("co2scrubber         = $co2scrubber")
 
-        if (buckets == null) {
-            buckets = List(it.count()) { BitFlags(0, 0) }
-        }
+    println("Life support rating = ${Integer.parseInt(oxygen, 2) * Integer.parseInt(co2scrubber, 2)}")
+}
 
+fun getOxygen(items: List<String>, commonBit: Char, index: Int = 0): String {
+    val buckets = getBuckets(items)
+    if (items.count() == 1) return items.first()
+    val result = items.filter {
+        it[index] == buckets[index].mostCommon(commonBit)
+    }
+
+    return getOxygen(result, commonBit, index + 1)
+}
+
+fun getCO2Scrubber(items: List<String>, commonBit: Char, index: Int = 0): String {
+    val buckets = getBuckets(items)
+    if (items.count() == 1) return items.first()
+    val result = items.filter {
+        it[index] == buckets[index].leastCommon(commonBit)
+    }
+
+    return getCO2Scrubber(result, commonBit, index + 1)
+}
+
+fun getBuckets(items: List<String>): List<BitFlags> {
+    val buckets = List(items.first().count()) { BitFlags(0, 0) }
+
+    items.forEach {
         it.forEachIndexed { index, c ->
             if (c == '0') {
-                buckets!![index].zeroes++
+                buckets[index].zeroes++
             } else if (c == '1') {
-                buckets!![index].ones++
+                buckets[index].ones++
             }
         }
     }
 
-    println(buckets)
-
-    buckets!!.forEach {
-        gammaRate += if (it.zeroesMostCommon()) "0" else "1"
-        epsilonRate += if (it.zeroesMostCommon()) "1" else "0"
-    }
-
-    println("Gamma = $gammaRate")
-    println("Epsilon = $epsilonRate")
-
-    println("Power consumption = ${Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2)}")
+    return buckets
 }
